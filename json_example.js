d2fa0eb7532f665e769727dda7713d6f8d24205c
Nur Ariffin Mohd Zin <nurariffinmohdzin@gmail.com>
2024-11-18 14:51:59 +0800

    first commit

---
+++ json_example.js
@@ -0,0 +1,15 @@
+let student = {
+
+    "name":"Abu",
+    "age":20,
+    "program":"BIP",
+    "faculty":"FSKTM",
+    "skills":["C++","Java","PHP"],
+    "friend":{"name":"Ali","age":20}   
+};
+
+let studentName = student.name;
+console.log(studentName);
+
+let skillJava = student.skills[1];
+console.log(skillJava);
